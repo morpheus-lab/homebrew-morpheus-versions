f46e78e761f883e6a93f304b44e7b97ea0128cd6
Diego Jahn <jahndiego@mailbox.org>
2025-09-26 13:53:02 +0200

    morpheus-release: refactor dependencies and installation process
    - Move `libomp` dependency to macOS-specific block for consistency with Homebrew/homebrew-core
    - Add resources for `xtensor`, `xsimd`, and `xtl` with specific versions
    - Update installation method to handle new resources and avoid static linking to Boost for consistency with Homebrew/homebrew-core
    - Implement post-install code signing for macOS app bundle for consistency with Homebrew/homebrew-core
    - Modify test XML formatting

--- Formula/morpheus-release.rb
+++ Formula/morpheus-release.rb
@@ -10,10 +10,8 @@ class MorpheusRelease < Formula
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
   depends_on "ninja" => :build
-  depends_on "xsimd" => :build
   depends_on "gnuplot"
   depends_on "graphviz"
-  depends_on "libomp"
   depends_on "libtiff"
   depends_on "qt@5"
   depends_on "ffmpeg" => :recommended # Runtime dependencies
@@ -22,14 +20,79 @@ class MorpheusRelease < Formula
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "libomp"
+  end
+
+  resource "xtensor" do
+    url "https://github.com/xtensor-stack/xtensor/archive/refs/tags/0.27.0.tar.gz"
+    sha256 "9ca1743048492edfcc841bbe01f58520ff9c595ec587c0e7dc2fc39deeef3e04"
+  end
+
+  resource "xsimd" do
+    url "https://github.com/xtensor-stack/xsimd/archive/refs/tags/13.2.0.tar.gz"
+    sha256 "edd8cd3d548c185adc70321c53c36df41abe64c1fe2c67bc6d93c3ecda82447a"
+  end
+
+  resource "xtl" do
+    url "https://github.com/xtensor-stack/xtl/archive/refs/tags/0.8.0.tar.gz"
+    sha256 "ee38153b7dd0ec84cee3361f5488a4e7e6ddd26392612ac8821cbc76e740273a"
+  end
+
   def install
-    # has to build with Ninja until: https://gitlab.kitware.com/cmake/cmake/-/issues/25142
-    args = ["-G Ninja"]
+    resource("xtl").stage do
+      mkdir "build" do
+        system "cmake", "-S", "..", "-B", ".",
+               "-DBUILD_TESTS=OFF",
+               *std_cmake_args(install_prefix: buildpath.to_s)
+        system "cmake", "--build", "."
+        system "cmake", "--install", "."
+      end
+    end
+
+    resource("xsimd").stage do
+      mkdir "build" do
+        system "cmake", "-S", "..", "-B", ".",
+               "-DBUILD_TESTS=OFF",
+               *std_cmake_args(install_prefix: buildpath.to_s)
+        system "cmake", "--build", "."
+        system "cmake", "--install", "."
+      end
+    end
+
+    resource("xtensor").stage do
+      mkdir "build" do
+        system "cmake", "-S", "..", "-B", ".",
+               "-DXTENSOR_USE_OPENMP=ON",
+               "-DXTENSOR_USE_SIMD=ON",
+               "-DBUILD_TESTS=OFF",
+               *std_cmake_args(install_prefix: buildpath.to_s)
+        system "cmake", "--build", "."
+        system "cmake", "--install", "."
+      end
+    end
+
+    # Avoid statically linking to Boost libraries when `-DBUILD_TESTING=OFF`
+    cmakelists = ["CMakeLists.txt", "morpheus/CMakeLists.txt"]
+    inreplace cmakelists, "set(Boost_USE_STATIC_LIBS ON)", "set(Boost_USE_STATIC_LIBS OFF)"
+
+    # Workaround for newer Clang
+    # error: a template argument list is expected after a name prefixed by the template keyword
+    ENV.append_to_cflags "-Wno-missing-template-arg-list-after-template-kw" if OS.mac?
+
+    # use branch name and Git commit hash as binary suffix
+    binary_suffix = "#{self.class.head.specs[:branch]}-#{`git rev-parse HEAD`.strip[0, 7]}"
+
+    args = [
+      "-G",
+      "Ninja", # has to build with Ninja until: https://gitlab.kitware.com/cmake/cmake/-/issues/25142
+      "-DMORPHEUS_BINARY_SUFFIX=#{binary_suffix}",
+      "-DCMAKE_PREFIX_PATH=#{buildpath}/resources",
+    ]
 
     if OS.mac?
       args << "-DMORPHEUS_RELEASE_BUNDLE=ON"
       args << "-DBREW_FORMULA_DEPLOYMENT=ON"
-      args << "-DMORPHEUS_BINARY_SUFFIX=#{self.class.head.specs[:branch]}-#{`git rev-parse HEAD`.strip[0, 7]}" # Append branch name and Git commit hash to binary name
 
       # SBML import currently disabled by default due to libSBML build errors with some macOS SDKs
       args << "-DMORPHEUS_SBML=OFF" if build.without? "sbml"
@@ -41,11 +104,16 @@ class MorpheusRelease < Formula
 
     return unless OS.mac?
 
-    bin.write_exec_script "#{prefix}/Morpheus.app/Contents/MacOS/morpheus"
-    bin.write_exec_script "#{prefix}/Morpheus.app/Contents/MacOS/morpheus-gui"
+    bin.install_symlink "#{prefix}/Morpheus.app/Contents/MacOS/morpheus-#{binary_suffix}" => "morpheus"
+    bin.install_symlink "#{prefix}/Morpheus.app/Contents/MacOS/morpheus-#{binary_suffix}-gui" => "morpheus-gui"
 
     # Set PATH environment variable including Homebrew prefix in macOS app bundle
     inreplace "#{prefix}/Morpheus.app/Contents/Info.plist", "HOMEBREW_BIN_PATH", "#{HOMEBREW_PREFIX}/bin"
+  end
+
+  def post_install
+    # Sign to ensure proper execution of the app bundle
+    system "/usr/bin/codesign", "-f", "-s", "-", "--deep", "#{prefix}/Morpheus.app" if OS.mac?
   end
 
   def caveats
@@ -65,7 +133,7 @@ class MorpheusRelease < Formula
   end
 
   test do
-    (testpath/"test.xml").write <<~EOF
+    (testpath/"test.xml").write <<~XML
       <?xml version='1.0' encoding='UTF-8'?>
       <MorpheusModel version="4">
           <Description>
@@ -90,7 +158,7 @@ class MorpheusRelease < Formula
               <ModelGraph include-tags="#untagged" format="dot" reduced="false"/>
           </Analysis>
       </MorpheusModel>
-    EOF
+    XML
 
     assert_match "Simulation finished", shell_output("#{bin}/morpheus --file test.xml")
   end
